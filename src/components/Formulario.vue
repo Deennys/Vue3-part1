61e2b245dd90f628b35a3bd193dfb374d331a760
Dennys Alvarenga <dennysnascimento01@gmail.com>
2022-12-22 19:07:27 -0300

    stopwatch logic finished

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -34,7 +34,8 @@ export default defineComponent({
     name: 'Formulário',
     data () {
         return {
-            tempoEmSegundos: 0
+            tempoEmSegundos: 0,
+            cronometro: 0
         }
     },
     computed: {
@@ -44,13 +45,12 @@ export default defineComponent({
     },
     methods: {
         iniciar() {
-            setInterval(() => {
+            this.cronometro = setInterval(() => {
                 this.tempoEmSegundos += 1;
             }, 1000)
-            console.log('iniciando');
         },
         finalizar() {
-            console.log('finalizando');
+            clearInterval(this.cronometro)
         }
     }
 });
