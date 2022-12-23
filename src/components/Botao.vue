79cc04ca22cd9ad39ae8a8230eb9b778afbf21c8
Dennys Alvarenga <dennysnascimento01@gmail.com>
2022-12-23 20:25:09 -0300

    Create Botao.vue

---
+++ src/components/Botao.vue
@@ -0,0 +1,33 @@
+<template>
+    <button class="button" @click="clicado" :disabled="cronometroRodando">
+        <span class="icon">
+            <i :class=icone></i>
+        </span>
+        <span>{{ valor }}</span>
+    </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Botao',
+    emits: ['clicado'],
+    props: {
+        icone: {
+            type: String
+        },
+        valor: {
+            type: String
+        },
+        cronometroRodando: {
+            type: Boolean
+        }
+    },
+    methods: {
+        clicado():void {
+            this.$emit('clicado');
+        }
+    }
+}) 
+</script>
--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,29 +1,22 @@
 <template>
     <div class="is-flex is-align-items-center is-justify-content-space-between">
         <Cronometro :tempoEmSegundos="tempoEmSegundos" />
-        <button class="button" @click="iniciar" :disabled="cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-        </button>
-        <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-        </button>
+        <Botao @clicado="iniciar" :cronometroRodando="cronometroRodando" icone="fas fa-play" valor="Play" />
+        <Botao @clicado="finalizar" :cronometroRodando="!cronometroRodando" icone="fas fa-stop" valor="Stop" />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Cronometro from './Cronometro.vue';
+import Botao from './Botao.vue';
 
 export default defineComponent({
     name: 'Temporizador',
+    emits: ['aoTemporizadorFinalizado'],
     components: {
-        Cronometro
+        Cronometro,
+        Botao
     },
     data () {
         return {
@@ -41,7 +34,9 @@ export default defineComponent({
         },
         finalizar() {
             this.cronometroRodando = false;
-            clearInterval(this.cronometro)
+            clearInterval(this.cronometro);
+            this.$emit('aoTemporizadorFinalizado', this.tempoEmSegundos);
+            this.tempoEmSegundos = 0;
         }
     }
 });
